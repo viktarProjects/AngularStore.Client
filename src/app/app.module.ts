710f6bf3951961e67b8ef5fcb853e4058e84d45e
viktar33 <viktar.varabei@mail.ru>
2023-01-18 17:25:00 +0300

    Added products service, filter and pagination

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -2,14 +2,20 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 import { AppComponent } from './app.component';
 import {BrowserAnimationsModule} from "@angular/platform-browser/animations";
+import {HttpClientModule} from "@angular/common/http";
+import {CoreModule} from "./core/core.module";
+import {ShopModule} from "./shop/shop.module";
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
   ],
   imports: [
     BrowserModule,
-    BrowserAnimationsModule
+    BrowserAnimationsModule,
+    HttpClientModule,
+    CoreModule,
+    ShopModule
   ],
   providers: [],
   bootstrap: [AppComponent]
---
+++ src/app/shared/models/shopParams.ts
@@ -0,0 +1,10 @@
+import {empty} from "rxjs";
+
+export class ShopParams {
+  brandId: number = 0;
+  typeId: number = 0;
+  sort: string = 'name';
+  pageNumber: number = 1;
+  pageSize: number = 6;
+  search: string | undefined = "";
+}
